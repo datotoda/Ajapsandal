d52022262ed7bab476d8d8a41ba9a8914ec2508d
datotoda <datotoda1342@gmail.com>
2022-05-31 18:16:29 +0400

    ბაზების გამართვა

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = 'text'
@@ -15,24 +16,26 @@ class Joke(db.Model):
     icon_url = db.Column(db.TEXT, nullable=False)
     updated_at = db.Column(db.TEXT, nullable=False)
     created_at = db.Column(db.TEXT, nullable=False)
+    comments = db.relationship('Comment', backref='joke')
 
 
 class Comment(db.Model):
-    id = db.Column(db.INT, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     value = db.Column(db.TEXT, nullable=False)
-    author = db.Column(db.TEXT, nullable=False)
-    joke = db.Column(db.TEXT, nullable=False)
-    created_at = db.Column(db.TEXT, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    joke_id = db.Column(db.TEXT, db.ForeignKey('joke.id'), nullable=False)
+    created_at = db.Column(db.TEXT, default=str(datetime.now()), nullable=False)
 
 
 class User(db.Model):
-    id = db.Column(db.INT, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.TEXT, nullable=False)
-    first_name = db.Column(db.TEXT, nullable=False)
-    last_name = db.Column(db.TEXT, nullable=False)
-    email = db.Column(db.TEXT, nullable=False)
+    first_name = db.Column(db.TEXT)
+    last_name = db.Column(db.TEXT)
+    email = db.Column(db.TEXT)
     password = db.Column(db.TEXT, nullable=False)
-    created_at = db.Column(db.TEXT, nullable=False)
+    created_at = db.Column(db.TEXT, default=str(datetime.now()), nullable=False)
+    comments = db.relationship('Comment', backref='author')
 
 
 @app.route('/')
@@ -66,4 +69,5 @@ def profile():
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
