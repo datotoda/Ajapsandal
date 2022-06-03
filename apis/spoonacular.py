718ff9a660593751fed17e2bff4384fbbac587f6
datotoda <datotoda1342@gmail.com>
2022-06-03 18:40:42 +0400

    add default recipes

--- apis/spoonacular.py
+++ apis/spoonacular.py
@@ -4,33 +4,28 @@ import json
 
 
 def get_receipts(query):
-    # return []
-    # task 3
-    # spoonacular.com-იდან API-ს მეშვეობით მომაქვს ჩემთვის სასურველი კერძის რეცეპტი.
-    key = '15d3f9db070e4b7980dd731441e1b04f'
-    number = 10
-    payload = {'apiKey': key, 'query': query, 'number': number}
-    r = requests.get(f'https://api.spoonacular.com/recipes/complexSearch', params=payload)
-    res = json.loads(r.text)
+    try:
+        # spoonacular.com-იდან API-ს მეშვეობით მომაქვს ჩემთვის სასურველი კერძის რეცეპტი.
+        key = '15d3f9db070e4b7980dd731441e1b04f'
+        number = 10
+        payload = {'apiKey': key, 'query': query, 'number': number}
+        r = requests.get(f'https://api.spoonacular.com/recipes/complexSearch', params=payload)
+        res = json.loads(r.text)
 
-    # return res
+        ids = []
+        for i in res['results']:
+            ids.append(str(i['id']))
+        ids = ','.join(ids)
+        payload = {'apiKey': key, 'ids': ids}
 
-    ids = []
-    for i in res['results']:
-        ids.append(str(i['id']))
-    ids = ','.join(ids)
-    payload = {'apiKey': key, 'ids': ids}
+        r2 = requests.get(f'https://api.spoonacular.com/recipes/informationBulk', params=payload)
+        res2 = json.loads(r2.text)
+        return res2
+    except Exception:
+        pass
 
-    r2 = requests.get(f'https://api.spoonacular.com/recipes/informationBulk', params=payload)
-    res2 = json.loads(r2.text)
-    return res2
-
-    # title = res2[0]['title']
-    # print(title)
-
-    # title = res['results'][0]['title']
-    # fat = res['results'][0]['nutrition']['nutrients'][0]['amount']
-    # return title  # დაბეჭდავს კერძის დასახელებას
+    with open('apis/default_recipes.json', 'r') as f:
+        return json.load(f)
 
 
 if __name__ == '__main__':
