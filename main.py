7754a7ededb39b0f7852f70826e2793d603b3b80
datotoda <datotoda1342@gmail.com>
2022-06-03 15:23:30 +0400

    პროფილის გვერდის ფუნქციონალი

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash, abort
+from flask.views import MethodView
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from apis import chucknorris, spoonacular
@@ -65,31 +66,31 @@ def index():
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if 'user_id' in session:
-        return redirect(url_for('profile'))
+        return redirect(url_for('profile', user_id=session['user_id']))
 
     if request.method == 'POST':
         username = request.form.get('username', '')
         password = request.form.get('password', '')
 
         if not username:
-            flash('Input username', 'u_err')
+            flash('Input username', 'username_err')
         if not password:
-            flash('Input password', 'p_err')
+            flash('Input password', 'password_err')
         if not (username and password):
             return render_template('login.html', username=username)
 
         user = User.query.filter_by(username=username).first()
 
         if not user:
-            flash('Incorrect username', 'u_err')
+            flash('Incorrect username', 'username_err')
             return redirect(url_for('login'))
 
         if user.password != password:
-            flash('Incorrect password', 'p_err')
+            flash('Incorrect password', 'password_err')
             return render_template('login.html', username=username)
 
         session['user_id'] = user.id
-        return redirect(url_for('profile'))
+        return redirect(url_for('profile', user_id=user.id))
 
     return render_template('login.html')
 
@@ -110,11 +111,64 @@ def registration():
     return render_template('registration.html')
 
 
-@app.route('/profile')
-def profile():
-    if 'user_id' not in session:
-        return redirect(url_for('login'))
-    return render_template('profile.html')
+class UserMethodView(MethodView):
+    def get(self, user_id):
+        if user_id is None:
+            return redirect(url_for('login'))
+
+        user = User.query.filter_by(id=user_id).first()
+        if not user:
+            abort(404)
+
+        return render_template('profile.html', user=user, is_editable=user.id == session.get('user_id', ''))
+
+    def post(self, user_id):
+        if user_id != session.get('user_id', ''):
+            return redirect(url_for('profile', user_id=user_id))
+
+        user = User.query.filter_by(id=user_id).first()
+
+        username, f_name, l_name, email = [request.form.get(key, '') for key in
+                                           ['username', 'first_name', 'last_name', 'email']]
+        old_pass, new_pass, conf_pass = [request.form.get(key, '') for key in
+                                         ['old_password', 'new_password', 'conf_password']]
+        if not username:
+            flash('Input username', 'username_err')
+        elif user.username != username and len(User.query.filter_by(username=username).all()) != 0:
+            flash('Username already exists', 'username_err')
+        else:
+            user.username = username
+
+        if f_name:
+            user.first_name = f_name
+        if l_name:
+            user.last_name = l_name
+
+        if email:
+            if email.count('@') != 1 or email.split('@')[-1].count('.') != 1:
+                flash('Incorrect email', 'email_err')
+            else:
+                user.email = email
+
+        if old_pass or new_pass or conf_pass:
+            if user.password != old_pass:
+                flash('Incorrect password', 'old_pass_err')
+            elif new_pass != conf_pass:
+                flash('Passwords not match', 'conf_pass_err')
+            elif not new_pass:
+                flash('Input new password', 'new_pass_err')
+            elif len(new_pass) < 8:
+                flash('Password must be at least 8 characters long', 'new_pass_err')
+            elif old_pass == new_pass:
+                flash('This is old password', 'new_pass_err')
+            else:
+                user.password = new_pass
+
+        db.session.commit()
+        return redirect(url_for('profile', user_id=user_id))
+
+
+app.add_url_rule('/profile/<int:user_id>', view_func=UserMethodView.as_view('profile'))
 
 
 @app.route('/recipes')
